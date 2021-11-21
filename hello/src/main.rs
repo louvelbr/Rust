81ba6aa345c715d0422d87fa3701d86f6eb789e9
Louve Le Bronec <loane.le.bronec@eisti.fr>
2021-11-21 15:50:45 +0000

    Simulating a slow reuest in the current server implementation

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,6 +2,9 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
 let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -18,8 +21,12 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -32,6 +39,6 @@ fn handle_connection(mut stream: TcpStream){
     );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-    }
-   // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
+   // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
